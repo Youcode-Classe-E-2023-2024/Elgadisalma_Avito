4e04b0678bc7659be474fbc20017ca786d2b7476
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-15 16:06:41 +0100

    Create ajout_annonce.php

---
+++ ajout_annonce.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'config.php';
+
+$nom_err = ''; 
+
+if (isset($_POST['submit'])) {
+    $nom_utilisateur = $_POST['userName'];
+    $numero_tel = $_POST['phoneNumber'];
+    $titre_annonce = $_POST['annoceTitre'];
+    $description_annonce = $_POST['annoceDescription'];
+    $date_ajout = $_POST['dateAjout'];
+
+    $sql_check_user = "SELECT id FROM annonce WHERE nom_utilisateur = '$nom_utilisateur'";
+    $result_check_user = $link->query($sql_check_user);
+
+    if ($result_check_user->num_rows > 0) {
+        $nom_err = 'Nom d\'utilisateur déjà existant.';
+        session_start();
+        $_SESSION['nom_err'] = $nom_err;
+        header("Location: index.php");
+        exit();
+    } else {
+        $sql_insert = "INSERT INTO annonce (nom_utilisateur, numero_tel, titre_annonce, description_annonce, date_ajout) 
+                       VALUES ('$nom_utilisateur', '$numero_tel', '$titre_annonce', '$description_annonce', '$date_ajout')";
+
+        if ($link->query($sql_insert) === TRUE) {
+            header("location:../annonce.php?task=added_successfully");
+            exit();
+        } else {
+            echo "Erreur: " . $link->error;
+        }
+    }
+
+    $link->close();
+}
+?>
