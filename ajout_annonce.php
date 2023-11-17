6be8b3029d6aeb3262a3c66211b9d05cdfd0c945
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-17 03:04:27 +0100

    Update ajout_annonce.php

--- ajout_annonce.php
+++ ajout_annonce.php
@@ -10,16 +10,16 @@ if (isset($_POST['submit'])) {
     $description_annonce = $_POST['annoceDescription'];
     $date_ajout = $_POST['dateAjout'];
 
-    $sql_check_user = "SELECT id FROM annonce WHERE nom_utilisateur = '$nom_utilisateur'";
-    $result_check_user = $link->query($sql_check_user);
+    // $sql_check_user = "SELECT id FROM annonce WHERE nom_utilisateur = '$nom_utilisateur'";
+    // $result_check_user = $link->query($sql_check_user);
 
-    if ($result_check_user->num_rows > 0) {
-        $nom_err = 'Nom d\'utilisateur déjà existant.';
-        session_start();
-        $_SESSION['nom_err'] = $nom_err;
-        header("Location: annonce.php?STATUS=Nom d'utilisateur déjà existant");
-        exit();
-    } else {
+    // if ($result_check_user->num_rows > 0) {
+    //     $nom_err = 'Nom d\'utilisateur déjà existant.';
+    //     session_start();
+    //     $_SESSION['nom_err'] = $nom_err;
+    //     header("Location: ../annonce.php?STATUS=Nom d'utilisateur déjà existant");
+    //     exit();
+    // } else {
         $sql_insert = "INSERT INTO annonce (nom_utilisateur, numero_tel, titre_annonce, description_annonce, date_ajout) 
                        VALUES ('$nom_utilisateur', '$numero_tel', '$titre_annonce', '$description_annonce', '$date_ajout')";
 
@@ -32,5 +32,5 @@ if (isset($_POST['submit'])) {
     }
 
     $link->close();
-}
+
 ?>
