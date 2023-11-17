7b8300d28a88c15848f63cd260ee77e643d44969
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-17 09:42:15 +0100

    Update ajout_annonce.php

--- ajout_annonce.php
+++ ajout_annonce.php
@@ -1,14 +1,14 @@
 <?php
+
 require_once 'config.php';
 
-$nom_err = ''; 
 
-if (isset($_POST['submit'])) {
-    $nom_utilisateur = $_POST['userName'];
-    $numero_tel = $_POST['phoneNumber'];
-    $titre_annonce = $_POST['annoceTitre'];
-    $description_annonce = $_POST['annoceDescription'];
-    $date_ajout = $_POST['dateAjout'];
+// if (isset($_POST['submit'])) {
+//     $nom_utilisateur = $_POST['userName'];
+//     $numero_tel = $_POST['phoneNumber'];
+//     $titre_annonce = $_POST['annoceTitre'];
+//     $description_annonce = $_POST['annoceDescription'];
+//     $date_ajout = $_POST['dateAjout'];
 
     // $sql_check_user = "SELECT id FROM annonce WHERE nom_utilisateur = '$nom_utilisateur'";
     // $result_check_user = $link->query($sql_check_user);
@@ -20,16 +20,34 @@ if (isset($_POST['submit'])) {
     //     header("Location: ../annonce.php?STATUS=Nom d'utilisateur déjà existant");
     //     exit();
     // } else {
-        $sql_insert = "INSERT INTO annonce (nom_utilisateur, numero_tel, titre_annonce, description_annonce, date_ajout) 
-                       VALUES ('$nom_utilisateur', '$numero_tel', '$titre_annonce', '$description_annonce', '$date_ajout')";
+    //     $sql_insert = "INSERT INTO annonce (nom_utilisateur, numero_tel, titre_annonce, description_annonce, date_ajout) 
+    //                    VALUES ('$nom_utilisateur', '$numero_tel', '$titre_annonce', '$description_annonce', '$date_ajout')";
 
-        if ($link->query($sql_insert) === TRUE) {
-            header("location:../annonce.php?task=added_successfully");
-            exit();
-        } else {
-            echo "Erreur: " . $link->error;
-        }
+    //     if ($link->query($sql_insert) === TRUE) {
+    //         header("location:../annonce.php?task=added_successfully");
+    //         exit();
+    //     } else {
+    //         echo "Erreur: " . $link->error;
+    //     }
+    // }
+
+if (isset($_POST['submit'])) {
+    $nom_utilisateur = $_POST['userName'];
+    $numero_tel = $_POST['phoneNumber'];
+    $titre_annonce = $_POST['annoceTitre'];
+    $description_annonce = $_POST['annoceDescription'];
+    $date_ajout = $_POST['dateAjout'];
+
+    $sql_insert = "INSERT INTO annonce (nom_utilisateur, numero_tel, titre_annonce, description_annonce, date_ajout) 
+                   VALUES ('$nom_utilisateur', '$numero_tel', '$titre_annonce', '$description_annonce', '$date_ajout')";
+
+    if ($link->query($sql_insert) === TRUE) {
+        header("location:../annonce.php?task=added_successfully");
+        exit();
+    } else {
+        echo "Erreur: " . $link->error;
     }
+}
 
     $link->close();
 
